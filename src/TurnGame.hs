8891957052d93446078159df68d9f330b2a893ea
Daniel Corson <danl@alum.mit.edu>
2009-03-11 17:33:11 -0700

    trying to find concrete plan for TurnGame

--- src/TurnGame.hs
+++ src/TurnGame.hs
@@ -5,13 +5,20 @@ module TurnGame where
 import Control.Monad.Error
 import qualified PomTree as PMT
 
--- a turn-based game such as go, chess, or chinese checkers
--- with branching history and possibly computer/network players
+-- a turn-based game such as go or chinese checkers, plus:
+-- - branching history
+-- - computer/network player support
 class TurnGame gm mv where
-  -- do a move and tell computer/network players
-  doMove    :: mv -> gm -> ErrorT String IO gm
+  -- do a move and tell computer/network players.
+  doMove :: mv -> gm -> ErrorT String IO gm
+  -- generate a move for the current player.
+  genMove :: gm -> ErrorT String IO mv
+  -- generate a computer move even if the current player is a human.
+  -- intended to be used by genMove or to get a "hint" from the engine.
+  compGenMove :: gm -> ErrorT String IO mv
+  whoseTurn :: gm -> player
+
   --readMove  :: String -> gm -> Either String mv
-  --whoseTurn :: gm -> player
   -- get a move from a computer/network player
   --compMove  :: gm -> ErrorT String IO (gm, mv)
   {-
@@ -23,4 +30,14 @@ class TurnGame gm mv where
   --playGame disp gm =
   -}
 
-  playGame  :: gm -> ErrorT String IO (PMT.OmTree mv, PMT.OmTreeContext mv)
+  playGame :: gm -> ErrorT String IO (PMT.OmTree mv, PMT.OmTreeContext mv)
+  playGame gm = do
+    getMove
+    compMove
+
+  -- this can be replaced with a more direct version (in the Go Text Protocol,
+  -- e.g., compGenMove needs to be 'genmove+undo' whereas compMove can just be
+  -- 'genmove').
+  -- intended to be used by doMove
+  compMove :: gm -> ErrorT String IO gm
+  compMove gm = flip doMove gm =<< compGenMove gm
